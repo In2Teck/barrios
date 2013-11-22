57362e66272d0cfb8865a73c7c7d949e622061a4
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-11-21 22:53:42 -0600

    Added all users hack

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -29,7 +29,7 @@ class DisplayController < ApplicationController
   end
 
   def csv_all_users
-    @users = User.all
+    @users = User.where("id > 0")
     respond_to do |format|
       format.csv { send_data @users.to_csv_all_users }
       format.xls
